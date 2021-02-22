3f4a9f968d98a92be8d53390a70589b233ff2fee
ARIEL GIMENEZ <arielgimenez_95@hotmail.com>
2021-02-22 16:23:06 -0300

    solucionado problema de que no funcioba colision en singleplayer al jugar contra rival

---
+++ Assets/Scripts/NivelDeJuegoScripts/CuadriculaDeColisionJugandoNivelSinglePlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CuadriculaDeColisionJugandoNivelSinglePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+        Gamehandler _GamehandlerRED;
+    
+    private void Awake()
+    {
+        _GamehandlerRED = FindObjectOfType<Gamehandler>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        if(_GamehandlerRED.GetPuedoPresionarBoton() == true)
+        {
+            // print("presione en el cubo");
+            this.GetComponent<MeshRenderer>().enabled = true;
+            _GamehandlerRED.IsTurnoEnemigo();
+        }
+
+    }
+}
