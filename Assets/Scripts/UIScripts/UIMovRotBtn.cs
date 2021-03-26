18bfc7a1e8a13d675c71fdbfb92b5453bc5f4291
bacordgames <bacordgames@gmail.com>
2021-03-26 18:01:24 -0300

    Boton para Mover/Rotar

---
+++ Assets/Scripts/UIScripts/UIMovRotBtn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIMovRotBtn : MonoBehaviour
+{
+    public static UIMovRotBtn instance;
+
+    [SerializeField] private Button btn;
+    [SerializeField] private TextMeshProUGUI txt;
+
+	private bool moveMode = true;
+
+	public bool MoveMode { get => moveMode; set => moveMode = value; }
+
+	private void Awake()
+	{
+		instance = this;
+	}
+
+	public void ToggleMode()
+	{
+		MoveMode = !MoveMode;
+		if (MoveMode)
+			txt.SetText("M");
+		else
+			txt.SetText("R");
+	}
+
+}
