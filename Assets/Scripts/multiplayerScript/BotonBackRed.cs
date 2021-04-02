34993a4db159842c9b2995576de05f34047f951b
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-04-01 23:33:56 -0300

    solucionando problema de los botones en multiplayer al reiniciar la partida, aun me faltan algunas cosas

--- Assets/Scripts/multiplayerScript/BotonBackRed.cs
+++ Assets/Scripts/multiplayerScript/BotonBackRed.cs
@@ -5,12 +5,20 @@ using UnityEngine.SceneManagement;
 
 using Photon.Pun;
 
-public class BotonBackRed : MonoBehaviour
+public class BotonBackRed : MonoBehaviour,IPunObservable
 {
+    public GameObject Ui_EnemyDisconect;
+    GameHandlerRED _GameHandlerRED;
     GameObject _GestorDeRed;
+    PhotonView _PhotonView;
+    AudioSource SonidoWinner;
+
     private void Awake()
     {
         _GestorDeRed = GameObject.Find("GestorDeRed");
+        _PhotonView = GetComponent<PhotonView>();
+        SonidoWinner = GameObject.Find("SonidoWinner").GetComponent<AudioSource>();
+        _GameHandlerRED = FindObjectOfType<GameHandlerRED>();
     }
     // Start is called before the first frame update
     void Start()
@@ -26,10 +34,44 @@ public class BotonBackRed : MonoBehaviour
 
     public void VolverYDesconectarDeRed()
     {
+        
         PhotonNetwork.Disconnect();//desconecto del sirvidor
         Destroy(_GestorDeRed);//destruyo el gestor de Red
+
         SceneManager.LoadScene("MenuInicio");//reinicio a menu de inicio
     }
 
-    
+    public void Volver_Desconectar_Y_Avisar_Enemigo()
+    {
+        StartCoroutine("DesconectarPhoton");
+    }
+
+    /// <summary>Desconecto todo lo relacionado a photon y aviso a rival, espero 1 segundo antes de desconectar</summary>
+    IEnumerator DesconectarPhoton()
+    {
+        //photon para desirle al que esta del otro lado
+        _PhotonView.RPC("PlayerDisconect",
+            RpcTarget.OthersBuffered
+        );
+        yield return new WaitForSeconds(0.5f);
+        PhotonNetwork.Disconnect();//desconecto del sirvidor
+        Destroy(_GestorDeRed);//destruyo el gestor de Red
+
+        SceneManager.LoadScene("MenuInicio");//reinicio a menu de inicio
+    }
+
+    /// <summary>Hace que muestre la pantalla enemigo Desconectado</summary>
+    [PunRPC]
+    void PlayerDisconect()
+    {
+        print("el rival volvio al menu de inicio, aparece pantalla enemyDisconect");
+        _GameHandlerRED.SetPuedoPresionarBoton(false);//no puedo presionar boton
+        SonidoWinner.Play();
+        Ui_EnemyDisconect.SetActive(true);//activo el enemigo
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        
+    }
 }
