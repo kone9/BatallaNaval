2bf5c2a66cfa6bc13d9fd2ceac5406c0ef431c81
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-04-01 21:47:35 -0300

    solucionado boton back en multiplayer, pero tengo que agregar una pantalla para avisarle al rival que se desconecto un jugador

---
+++ Assets/Scripts/multiplayerScript/BotonBackRed.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+using Photon.Pun;
+
+public class BotonBackRed : MonoBehaviour
+{
+    GameObject _GestorDeRed;
+    private void Awake()
+    {
+        _GestorDeRed = GameObject.Find("GestorDeRed");
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void VolverYDesconectarDeRed()
+    {
+        PhotonNetwork.Disconnect();//desconecto del sirvidor
+        Destroy(_GestorDeRed);//destruyo el gestor de Red
+        SceneManager.LoadScene("MenuInicio");//reinicio a menu de inicio
+    }
+
+    
+}
