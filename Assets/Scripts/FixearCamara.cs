3c7c542bc636df11ec7d95a222b676e85d7ed0f1
Ariel Gimenez <arielgimenez_95@hotmail.com>
2021-04-24 23:37:24 -0300

    solucionando problemas de posicion de la cámara dependiendo el tamaño de la pantalla

--- Assets/Scripts/FixearCamara.cs
+++ Assets/Scripts/FixearCamara.cs
@@ -5,16 +5,39 @@ using UnityEngine;
 public class FixearCamara : MonoBehaviour
 {
     public Transform target;
-    Camera cam;
+    Camera camaraActual;
 
-    void Start()
-    {
-        cam = GetComponent<Camera>();
+    float defaultwidth;
+
+    bool mainteneWidth = true;
+
+    private void Awake() {
+        camaraActual = GetComponent<Camera>();
+        defaultwidth = camaraActual.transform.position.y * Screen.width;
     }
-
     void Update()
     {
-        Vector3 screenPos = cam.WorldToScreenPoint(target.position);
-        Debug.Log("target is " + screenPos.x + " pixels from the left");
+        if(mainteneWidth)
+        {
+            camaraActual.transform.position = new Vector3(
+                this.transform.position.x,
+                defaultwidth / Screen.width,
+                this.transform.position.z
+            ); 
+                
+        }
+        else
+        {
+
+        }
+        // Vector3 screenPos = cam.WorldToScreenPoint(target.position);
+        // cam.transform.position = this.transform.position - screenPos;
+        // Debug.Log("target is " + screenPos.x + " pixels from the left");
+        // Vector3 pos = new Vector3(15, Screen.height - 15, 10);
+        // transform.position = Camera.main.ScreenToWorldPoint(pos);
+        // float posicionAnterior  = Screen.width;
+        // float diferencia = posicionAnterior + Screen.width;
+        // camaraActual.aspect = Screen.width;
+        // print("el tamaño de la pantalla en height: "  + diferencia);
     }
 }
